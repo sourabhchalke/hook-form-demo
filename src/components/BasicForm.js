7c3900595153b68d16aff2d874900e219ab7c3c1
sourabhchalke <sourabhchalke2157@gmail.com>
2024-08-14 18:31:35 +0530

    form validation added

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -5,32 +5,60 @@ import { DevTool } from '@hookform/devtools';
 
 export default function BasicForm() {
 
-const form = useForm()
-const {register,control,handleSubmit}=form;
+  const form = useForm()
+  const { register, control, handleSubmit } = form;
 
-const onSubmit=(data)=>{console.log("Form Submitted",data)}
+  const onSubmit = (data) => { console.log("Form Submitted", data) }
 
   return (
     <div className='row m-0'>
       <div className='col-12 col-md-8 col-lg-4 mx-auto mt-5'>
         <form onSubmit={handleSubmit(onSubmit)}>
-            <h3>SignIn Form</h3>
-            <div>
-                <label htmlFor='username'>Username : </label><br/>
-                <input type='text' id='username' {...register("username")} /><br/>
-            </div>
-            <div>
-                <label htmlFor='email'>Email : </label><br/>
-                <input type='email' id='email' {...register("email")}/><br/>
-            </div>
-            <div>
-                <label htmlFor='password'>Password : </label><br/>
-                <input type='password' id='password' {...register("password")} /><br/>
-            </div>
-            <button className='btn btn-danger px-5 py-2 mt-3'>Submit</button>
+          <h3>SignIn Form</h3>
+          <div>
+            <label htmlFor='username'>Username : </label><br />
+            <input type='text' id='username' {...register("username", {
+              required: {
+                value: true,
+                message: "Please Enter Your Name"
+              }
+            })} /><br />
+          </div>
+          <div>
+            <label htmlFor='email'>Email : </label><br />
+            <input type='email' id='email' {...register("email", {
+              required:{
+                value:true,
+                message:"Please Enter Email"
+              },
+              pattern: {
+                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/,
+                message:"Invalid Email"
+              },
+              
+            })} /><br />
+          </div>
+          <div>
+            <label htmlFor='password'>Password : </label><br />
+            <input type='password' id='password' {...register("password",{
+              required:{
+                value:true,
+                message:"Enter Password"
+              },
+              minLength:{
+                value:6,
+                message:"Invalid Minimum Length 6"
+              },
+              maxLength:{
+                value:10,
+                message:"Invalid Maximum Length 10"
+              }
+            })} /><br />
+          </div>
+          <button className='btn btn-danger px-5 py-2 mt-3'>Submit</button>
         </form>
       </div>
-      <DevTool control={control}/>
+      <DevTool control={control} />
     </div>
   )
 }
