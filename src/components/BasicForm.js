e67fc97e2d5ff37bdc51992ce7e6041ce37a1afa
sourabhchalke <sourabhchalke2157@gmail.com>
2024-08-14 17:44:32 +0530

    Managing Form State

---
+++ src/components/BasicForm.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+
+
+export default function BasicForm() {
+
+const form = useForm()
+const {register}=form;
+
+
+  return (
+    <div className='row m-0'>
+      <div className='col-12 col-md-8 col-lg-4 mx-auto mt-5'>
+        <form>
+         
+            <h3>SignIn Form</h3>
+            <div>
+                <label htmlFor='username'>Username : </label><br/>
+                <input type='text' id='username' {...register("username")} /><br/>
+            </div>
+            <div>
+                <label htmlFor='email'>Email : </label><br/>
+                <input type='email' id='email' {...register("email")}/><br/>
+            </div>
+            <div>
+                <label htmlFor='password'>Password : </label><br/>
+                <input type='password' id='password' {...register("password")} /><br/>
+            </div>
+            <button className='btn btn-danger px-5 py-2 mt-3'>Submit</button>
+        </form>
+      </div>
+      
+    </div>
+  )
+}
